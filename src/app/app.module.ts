da5e6e7d6e07c97554919d5d6dd0e4a7bec79dbe
benkivuva <benkivuvak5@gmail.com>
2024-05-08 10:30:50 +0300

    Resolved Angular Material issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './mock-data';
 
@@ -10,6 +11,9 @@ import { TaskDetailsComponent } from './components/task-details/task-details.com
 import { TaskFormComponent } from './components/task-form/task-form.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     declarations: [
@@ -21,9 +25,13 @@ import { AppRoutingModule } from './app-routing.module';
     imports: [
         BrowserModule,
         HttpClientModule,
+        FormsModule,
         HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 }),
         RouterModule.forRoot([]),
-        AppRoutingModule
+        AppRoutingModule,
+        MatIconModule,
+        MatTableModule,
+        MatFormFieldModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/components/task-details/task-details.component.ts
+++ src/app/components/task-details/task-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TaskService } from '../../task.service';
-import { Task } from '../task.model';
+import { Task } from '../../task.model';
 
 @Component({
   selector: 'app-task-details',
